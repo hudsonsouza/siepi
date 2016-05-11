fbf783f4eb3a26e1b55a2355bc42f1f88d6c1838
Hudson Souza <hudsonss@gmail.com>
2016-05-11 19:31:20 -0300

    siepi 0.0.0 - 11/05/2016 inicio

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php ?>
+<center>
+    <img src="img-int/siepi-cabecalho-transp.png" alt="Sistema de Calculos Epidemiologicos - SiEpi" title="Sistema de Calculos Epidemiologicos - SiEpi" />
+    <!-- 
+    <br>
+    <img src="img-int/siepi-logomarca-transp.png" alt="Sistema de Cálculos Epidemiológicos - SiEpi" title="Sistema de Cálculos Epidemiológicos - SiEpi"/>
+    <br>
+    <img src="img-int/siepi-logotipo-branco.png" alt="Sistema de Cálculos Epidemiológicos - SiEpi" title="Sistema de Cálculos Epidemiológicos - SiEpi"/>
+    -->
+    
+</center>
+
